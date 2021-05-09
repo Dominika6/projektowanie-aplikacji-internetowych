1b192de189eed7450beb2033e3f9c07d75adfe5d
Dominika6 <dominikajadach@o2.pl>
2021-05-09 19:48:39 +0200

    Last version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,8 @@
-// import React from 'react';
-// import './App.css';
-// import { Route } from "react-router-dom"
-// import {Container, Row, Col} from 'react-bootstrap';
-// import {BrowserRouter as Router} from 'react-router-dom';
-//
-// import LoggedNavigationBar from "./components/LoggedNavigationBar";
-// import NavigationBar from "./components/NavigationBar";
-// import UserWelcome from "./components/UserWelcome";
-// import Footer from "./components/Footer";
-// // import EditAccount from "./components/EditAccount";
-// // import AdminManageUserAccounts from "./components/AdminManageUserAccounts";
-// // import AdminManageSurveys from "./components/AdminManageSurveys";
-// import Switch from "react-bootstrap/cjs/Switch";
-// // import AdminAccount from "./components/AdminAccount";
-// import Login, {getIsLoggedAsAdmin, getIsLoggedIn, getIsLoggedAsUser} from "./components/Login";
-// // import NavigationBar2 from "./components/NavigationBar2";
-// // import WelcomeAdmin from "./components/WelcomeAdmin";
-// // import AdminResults from "./components/AdminResults";
-// // import Answers from "./components/Answers";
-
-// export default function App() {
-//
-//   const marginTop = {
-//     marginTop:"20px"
-//   };
-//
-//   const isLoggedIn= getIsLoggedIn();
-//   const isAdmin = getIsLoggedAsAdmin();
-//   const isUser= getIsLoggedAsUser();
-//
-//   return (
-//       <Router>
-//         {isLoggedIn && <NavigationBar/>}
-//         {!isLoggedIn && <LoggedNavigationBar/>}
-//         <Container>
-//           <Row>
-//             <Col lg={12} style={marginTop}>
-//               <Switch>
-//                 {!isLoggedIn && <Route path="/" exact component={Login}/>}
-//
-//                 {isLoggedIn && isUser && <>
-//                   <Route path="/" exact component={UserWelcome}/>
-//                   {/*<Route path="/editAccount" exact component={EditAccount}/>*/}
-//                   {/*<Route path="/castYourVote" exact component={Answers}/>*/}
-//                 </>}
-//
-//                 {isLoggedIn && isAdmin && <>
-//                   {/*<Route path="/" exact component={WelcomeAdmin}/>*/}
-//                   {/*<Route path="/admin" exact component={WelcomeAdmin}/>*/}
-//                   {/*<Route path="/admin/yourAccount" exact component={AdminAccount}/>*/}
-//                   {/*<Route path="/admin/manageUserAccounts" exact component={AdminManageUserAccounts}/>*/}
-//                   {/*<Route path="/admin/manageSurveys" exact component={AdminManageSurveys}/>*/}
-//                   {/*<Route path="/admin/surveyResults" exact component={AdminResults}/>*/}
-//                 </>}
-//               </Switch>
-//             </Col>
-//           </Row>
-//         </Container>
-//         <Footer/>
-//       </Router>
-//   );
-// }
-
 import React from 'react';
 import './App.css';
 import { Route } from "react-router-dom";
 import {Container, Row, Col} from 'react-bootstrap';
 import {BrowserRouter as Router} from 'react-router-dom';
-
 import Welcome from "./components/Welcome";
 import Login, {getIsLoggedAsAdmin, getIsLoggedAsUser, getIsLoggedIn} from "./components/Login";
 import NavigationBar from "./components/NavigationBar";
@@ -128,9 +63,7 @@ export default function App() {
           </Col>
         </Row>
       </Container>
-      {/*<Footer>*/}
-        <Footer/>
-      {/*</Footer>*/}
+      <Footer/>
     </Router>
   );
 }
